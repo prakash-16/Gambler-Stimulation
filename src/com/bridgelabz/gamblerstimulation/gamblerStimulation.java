52417b19d3647a47a1529003e7a5e8e7c516468c
Prakash-16 <goudprakash58@gmail.com>
2021-08-12 15:43:46 +0530

    [Prakash] - Intialize value of stake/day and bet/game in program

---
+++ src/com/bridgelabz/gamblerstimulation/gamblerStimulation.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.gamblerstimulation;
+
+public class gamblerStimulation {
+	
+	public static int stake_per_day = 100, betPerGame = 1;
+	public static void main(String[] args) {
+		
+	}
+
+}
