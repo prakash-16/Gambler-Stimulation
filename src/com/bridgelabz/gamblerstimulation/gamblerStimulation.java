ad94a37a0a8fa29b733ef9dcb01dc8afc2366451
Prakash-16 <goudprakash58@gmail.com>
2021-08-12 17:29:58 +0530

    [Prakash] - Added feature to calculate Win/Loss of 20days

--- src/com/bridgelabz/gamblerstimulation/gamblerStimulation.java
+++ src/com/bridgelabz/gamblerstimulation/gamblerStimulation.java
@@ -4,21 +4,31 @@ public class gamblerStimulation {
 	
 	public static int stake_per_day = 100, betPerGame = 1;
 	public static void main(String[] args) {
-		while(50 < stake_per_day && stake_per_day < 150) {
-			double betResult = Math.floor(Math.random() * 10) % 2;
-			if(betResult == 1.0) {
-				System.out.println("Congratulation !! you have won the bet");
-				stake_per_day++;
+		int i,won_amt=0,loss_amt=0;
+		for(i=0;i<=19;i++) {
+			while(stake_per_day > 50 && stake_per_day < 150) {
+				double betResult = Math.floor(Math.random() * 10) % 2;
+				if(betResult == 1.0) {
+					System.out.println("Congratulation !! you have won the bet");
+					stake_per_day++;
+				}
+				else {
+					System.out.println("You have lost the best, Better luck next time");
+					stake_per_day--;
+				}
+				
+			}
+			System.out.println("Your today's betting collection is :- " + stake_per_day);
+			System.out.println("You are resign for the day " + (i + 1));
+			if(stake_per_day < 100) {
+				loss_amt = loss_amt + (100 - stake_per_day); 
 			}
 			else {
-				System.out.println("You have lost the best, Better luck next time");
-				stake_per_day--;
+				won_amt = won_amt + (stake_per_day - 100);
 			}
-			
 		}
-		System.out.println("Your today's betting collection is :- " + stake_per_day);
-		System.out.println("You are resign for the day");
-		
+		System.out.println("The winning amount is :- " + won_amt);
+		System.out.println("The loss amount is :- " + loss_amt);
 	}
 
 }
