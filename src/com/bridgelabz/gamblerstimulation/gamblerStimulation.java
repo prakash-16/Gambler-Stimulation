0e65912a9c98c4e06af93565ed85b50f671154ac
Prakash-16 <goudprakash58@gmail.com>
2021-08-12 21:35:02 +0530

    [Prakash] - Added the feature to calculate no. of days  win/loss

--- src/com/bridgelabz/gamblerstimulation/gamblerStimulation.java
+++ src/com/bridgelabz/gamblerstimulation/gamblerStimulation.java
@@ -4,7 +4,7 @@ public class gamblerStimulation {
 	
 	public static int stake_per_day = 100, betPerGame = 1;
 	public static void main(String[] args) {
-		int i,won_amt=0,loss_amt=0;
+		int i,won_amt=0,loss_amt=0,countWin=0,countLoss=0;
 		for(i=0;i<=19;i++) {
 			while(stake_per_day > 50 && stake_per_day < 150) {
 				double betResult = Math.floor(Math.random() * 10) % 2;
@@ -22,13 +22,17 @@ public class gamblerStimulation {
 			System.out.println("You are resign for the day " + (i + 1));
 			if(stake_per_day < 100) {
 				loss_amt = loss_amt + (100 - stake_per_day); 
+				countLoss++;
 			}
 			else {
-				won_amt = won_amt + (stake_per_day - 100);
+				won_amt = won_amt + (stake_per_day + 100);
+				countWin++;
 			}
 		}
 		System.out.println("The winning amount is :- " + won_amt);
 		System.out.println("The loss amount is :- " + loss_amt);
+		System.out.println("The winning days is :- " + countWin);
+		System.out.println("The loss days is :- " + countLoss);
 	}
 
 }
