8915b59aeea1a8ea7f42a7a041e41b33ac8de3c0
Prakash-16 <goudprakash58@gmail.com>
2021-08-12 15:58:36 +0530

    [Prakash] - Added caluculative gambler condition

--- src/com/bridgelabz/gamblerstimulation/gamblerStimulation.java
+++ src/com/bridgelabz/gamblerstimulation/gamblerStimulation.java
@@ -4,14 +4,20 @@ public class gamblerStimulation {
 	
 	public static int stake_per_day = 100, betPerGame = 1;
 	public static void main(String[] args) {
-		
-		double betResult = Math.floor(Math.random() * 10) % 2;
-		if(betResult == 1.0) {
-			System.out.println("Congratulation !! you have won the bet");
+		while(50 < stake_per_day && stake_per_day < 150) {
+			double betResult = Math.floor(Math.random() * 10) % 2;
+			if(betResult == 1.0) {
+				System.out.println("Congratulation !! you have won the bet");
+				stake_per_day++;
+			}
+			else {
+				System.out.println("You have lost the best, Better luck next time");
+				stake_per_day--;
+			}
+			
 		}
-		else {
-			System.out.println("You have lost the best, Better luck next time");
-		}
+		System.out.println("Your today's betting collection is :- " + stake_per_day);
+		System.out.println("You are resign for the day");
 		
 	}
 
